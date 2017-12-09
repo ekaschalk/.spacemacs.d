fd03ff8635c9ed46e03ced028ede0e73f2d01244
Eric Kaschalk <ekaschalk@gmail.com>
2017-12-09 14:50:18 -0800

    Clean: config layer rewrite complete, funcs.el clpsed, gnus/eshell

--- layers/config/local/evil-config/evil-config.el
+++ layers/config/local/evil-config/evil-config.el
@@ -8,13 +8,26 @@
 (setq evil-escape-unordered-key-sequence
       "true")
 
+(defun evil-scroll-to-center-advice (&rest args)
+  "Scroll line to center, for advising functions."
+  (evil-scroll-line-to-center (line-number-at-pos)))
+
 (advice-add 'evil-ex-search-next
-            :after 'config/scroll-to-center-advice)
+            :after 'evil-scroll-to-center-advice)
 (advice-add 'evil-ex-search-previous
-            :after 'config/scroll-to-center-advice)
+            :after 'evil-scroll-to-center-advice)
+
+(defun evil-execute-q-macro ()
+  "Execute @q"
+  (interactive)
+  (evil-execute-macro 1 "@q"))
 
 (evil-global-set-keys
  '(normal visual motion)
  "H" 'evil-first-non-blank
  "L" (lambda () (interactive) (evil-end-of-line))
  "0" 'evil-jump-item)
+
+(evil-global-set-key
+ 'normal
+ "Q" 'evil-execute-q-macro)
--- layers/config/packages.el
+++ layers/config/packages.el
@@ -2,121 +2,54 @@
 
 (setq config-packages
       '(
-        ;; Navigation
+        ;; Owned packages
         outshine
-        projectile
 
-        ;; Misc
+        ;; Elsewhere-owned packages with trivial config
         ispell
-        gnus
+        projectile
         yasnippet
 
         ;; Elsehwere-owned packages
-        (avy-config :location local)
-        (ivy-config :location local)
-        (evil-config :location local)
-        (org-config :location local)
+        (avy-config      :location local)
+        (eshell-config   :location local)
+        (evil-config     :location local)
+        (gnus-config     :location local)
+        (ivy-config      :location local)
+        (org-config      :location local)
         (treemacs-config :location local)
         ))
 
-
-;;; Navigation
-;;;; Outshine
-
-(defun config/init-outshine ()
-  (use-package outshine
-    :init
-    (progn
-      (spacemacs/set-leader-keys
-        "nn" 'outshine-narrow-to-subtree
-        "nw" 'widen)
-
-      (let ((kmap outline-minor-mode-map))
-        (define-key kmap (kbd "M-RET") 'outshine-insert-heading)
-        (define-key kmap (kbd "<backtab>") 'outshine-cycle-buffer)))
-
-    :config
-    (progn
-      ;; Narrowing works within the headline rather than requiring to be on it
-      (advice-add 'outshine-narrow-to-subtree :before
-                  (lambda (&rest args) (unless (outline-on-heading-p t)
-                                    (outline-previous-visible-heading 1))))
-
-      (add-hook 'outline-minor-mode-hook 'outshine-hook-function)
-      (add-hook 'prog-mode-hook 'outline-minor-mode))))
-
-;;;; Projectile
-
-(defun config/post-init-projectile ()
-  (setq projectile-indexing-method 'native))
-
-;;; Misc
-;;;; Ispell
+;;; Minor Config
 
 (defun config/post-init-ispell ()
-  (setq ispell-program-name "aspell"))
+  (setq ispell-program-name
+        "aspell"))
 
-;;;; Gnus
-
-(defun config/post-init-gnus ()
-  (setq user-mail-address	"ekaschalk@gmail.com"
-        user-full-name	"Eric Kaschalk"
-
-        ;; Get mail
-        gnus-secondary-select-methods
-        '((nnimap "gmail"
-                  (nnimap-address "imap.gmail.com")
-                  (nnimap-server-port 993)
-                  (nnimap-stream ssl))
-          (nntp "gmane"
-                (nntp-address "news.gmane.org"))
-          (nntp "news.gwene.org"))
-
-        ;; Send mail
-        message-send-mail-function 'smtpmail-send-it
-
-        ;; Archive outgoing email in Sent folder on imap.gmail.com
-        gnus-message-archive-method '(nnimap "imap.gmail.com")
-        gnus-message-archive-group "[Gmail]/Sent Mail"
-
-        ;; Auth
-        smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-        smtpmail-auth-credentials '(("smtp.gmail.com" 587
-                                     "ekaschalk@gmail.com" nil))
-
-        ;; SMPT Server config
-        smtpmail-default-smtp-server "smtp.gmail.com"
-        smtpmail-smtp-server "smtp.gmail.com"
-        smtpmail-smtp-service 587
-
-        ;; set return email address based on incoming email address
-        gnus-posting-styles
-        '(((header "to" "address@outlook.com")
-           (address  "address@outlook.com"))
-          ((header "to" "address@gmail.com")
-           (address "address@gmail.com")))
-
-        ;; store email in ~/gmail directory
-        nnml-directory "~/gmail"
-        message-directory "~/gmail"
-
-        ;; Full size images
-        mm-inline-large-images 'resize))
-
-;;;; Yasnippet
+(defun config/post-init-projectile ()
+  (setq projectile-indexing-method
+        'native))
 
 (defun config/pre-init-yasnippet ()
   (global-set-key (kbd "C-SPC") 'hippie-expand))
 
-;;; Local
+;;; Local Config
 
 (defun config/init-avy-config ()
   (use-package avy-config
     :after avy macros))
 
+(defun config/init-eshell-config ()
+  (use-package eshell-config
+    :after evil macros))
+
 (defun config/init-evil-config ()
   (use-package evil-config
     :after evil macros))
+
+(defun config/init-gnus-config ()
+  (use-package gnus-config
+    :after gnus))
 
 (defun config/init-ivy-config ()
   (use-package ivy-config
@@ -129,3 +62,30 @@
 (defun config/init-treemacs-config ()
   (use-package treemacs-config
     :after treemacs treemacs-evil macros))
+
+;;; Outshine
+
+(defun config/init-outshine ()
+  (defun advise-outshine-narrow-start-pos ()
+    (unless (outline-on-heading-p t)
+      (outline-previous-visible-heading 1)))
+
+  (use-package outshine
+    :after macros
+    :init
+    (progn
+      (spacemacs/set-leader-keys
+        "nn" 'outshine-narrow-to-subtree
+        "nw" 'widen)
+      (define-keys outline-minor-mode-map
+        (kbd "M-RET") 'outshine-insert-heading
+        (kbd "<backtab>") 'outshine-cycle-buffer))
+
+    :config
+    (progn
+      ;; Narrowing works within the headline rather than requiring to be on it
+      (advice-add 'outshine-narrow-to-subtree :before
+                  'advise-outshine-narrow-start-pos)
+
+      (add-hook 'outline-minor-mode-hook 'outshine-hook-function)
+      (add-hook 'prog-mode-hook 'outline-minor-mode))))
