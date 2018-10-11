429bcd0c7ed06e26c37978ea2b174264bae9627f
Eric Kaschalk <ekas@Erics-MacBook-Pro.local>
2018-10-11 14:15:17 -0700

    Clean: Remove unused ligatures, stdize display pkgs formatting

--- layers/display/local/pretty-code/pretty-code.el
+++ layers/display/local/pretty-code/pretty-code.el
@@ -1,10 +1,20 @@
 ;;; -*- lexical-binding: t -*-
 
+;; Note: I'm not using many of the symbols I've defined
+;; in the past like ones for "in", "for", and so on.
+;; This is because I got annoyed with indentation and that
+;; they aren't proportional.
+;; I love ligatures but only these remain in my workflow.
+;; It is a mammoth effort to fix both the above issues
+;; which I have explorered various times but do not have
+;; a comprehensive solution.
 
 (require 'prettify-utils)
 (require 'macros)
 
 (provide 'pretty-code)
+
+(global-prettify-symbols-mode 1)
 
 ;;; Config
 
--- layers/display/packages.el
+++ layers/display/packages.el
@@ -2,7 +2,7 @@
 
 (setq display-packages
       '(
-        ;; Owned Display Packages
+        ;; Owned packages
         all-the-icons
         all-the-icons-ivy
         all-the-icons-dired
@@ -11,255 +11,186 @@
         (prettify-utils :location (recipe :fetcher github
                                           :repo "Ilazki/prettify-utils.el"))
 
-        ;; Owned Local Display Packages
+        ;; Personal display-related packages
         (pretty-code :location local)
         (pretty-eshell :location local)
         (pretty-fonts :location local)
         (pretty-magit :location local)
         (pretty-outlines :location local)
-        ;; (windows-frame-size-fix :location local)
         ))
 
 ;;; Locals
 ;;;; Pretty-code
 
 (defun display/init-pretty-code ()
+  ;; Note: I'm not using many of the symbols I've defined
+  ;; in the past like ones for "in", "for", and so on.
+  ;; Full commentary on why in `pretty-code.el'.
+
   (use-package pretty-code
     :after prettify-utils macros
-    :config
-    (progn
-      (global-prettify-symbols-mode 1)
 
-      (setq hy-pretty-pairs
-            (pretty-code-get-pairs
-             '(;; Functional
-               :lambda
-               "fn"
-               :def "defn"
-
-               ;; Types
-               :null "None"
-
-               ;; Flow
-               :not "not" :in "in" :not-in "not-in"
-
-               ;; Other
-               :tuple "#t"  ; Tag macro for tuple casting
-               )))
-
-      (setq python-pretty-pairs
-            (pretty-code-get-pairs
-             '(;; Functional
-               :lambda
-               "lambda"
-               :def "def"
-
-               ;; Types
-               ;; :null "None"
-               ;; :int "int" :float "float"
-               ;; :str "str" :bool "bool"
-
-               ;; Flow
-               ;; :not "not"
-               ;; :in "in" :not-in "not in"
-               ;; :and "and" :or "or"
-               ;; :for "for"
-               ;; :return "return" :yield "yield"
-
-               ;; Other
-               ;; :tuple "Tuple" :pipe "tz-pipe"
-               )))
-
-      (setq emacs-lisp-pretty-pairs
-            (pretty-code-get-pairs
-             '(:def "defun")))
-
-      (pretty-code-set-pairs `((hy-mode-hook         ,hy-pretty-pairs)
-                               (python-mode-hook     ,python-pretty-pairs)
-                               (emacs-lisp-mode-hook ,emacs-lisp-pretty-pairs))))))
+    :config (pretty-code-set-pairs
+             `((hy-mode-hook
+                ,(pretty-code-get-pairs '(:lambda "fn" :def "defn")))
+               (python-mode-hook
+                ,(pretty-code-get-pairs '(:lambda "lambda" :def "def")))
+               (emacs-lisp-mode-hook
+                ,(pretty-code-get-pairs '(:def "defun")))))))
 
 ;;;; Pretty-eshell
 
 (defun display/init-pretty-eshell ()
   (use-package pretty-eshell
     :after macros
-    :config
-    (progn
-      ;; Directory
-      (pretty-eshell-section
-       esh-dir
-       "\xf07c"  ; 
-       (abbreviate-file-name (eshell/pwd))
-       '(:foreground "gold" :bold ultra-bold :underline t))
 
-      ;; Git Branch
-      (pretty-eshell-section
-       esh-git
-       "\xe907"  ; 
-       (magit-get-current-branch)
-       '(:foreground "pink"))
+    :config (progn
+              ;; Directory
+              (pretty-eshell-section
+               esh-dir
+               "\xf07c"  ; 
+               (abbreviate-file-name (eshell/pwd))
+               '(:foreground "gold" :bold ultra-bold :underline t))
 
-      ;; Python Virtual Environment
-      (pretty-eshell-section
-       esh-python
-       "\xe928"  ; 
-       pyvenv-virtual-env-name)
+              ;; Git Branch
+              (pretty-eshell-section
+               esh-git
+               "\xe907"  ; 
+               (magit-get-current-branch)
+               '(:foreground "pink"))
 
-      ;; Time
-      (pretty-eshell-section
-       esh-clock
-       "\xf017"  ; 
-       (format-time-string "%H:%M" (current-time))
-       '(:foreground "forest green"))
+              ;; Python Virtual Environment
+              (pretty-eshell-section
+               esh-python
+               "\xe928"  ; 
+               pyvenv-virtual-env-name)
 
-      ;; Prompy Number
-      (pretty-eshell-section
-       esh-num
-       "\xf0c9"  ; 
-       (number-to-string pretty-eshell-prompt-num)
-       '(:foreground "brown"))
+              ;; Time
+              (pretty-eshell-section
+               esh-clock
+               "\xf017"  ; 
+               (format-time-string "%H:%M" (current-time))
+               '(:foreground "forest green"))
 
-      (setq pretty-eshell-funcs
-            (list esh-dir esh-git esh-python esh-clock esh-num)))))
+              ;; Prompt Number
+              (pretty-eshell-section
+               esh-num
+               "\xf0c9"  ; 
+               (number-to-string pretty-eshell-prompt-num)
+               '(:foreground "brown"))
+
+              (setq pretty-eshell-funcs
+                    (list esh-dir esh-git esh-python esh-clock esh-num)))))
 
 ;;;; Pretty-fonts
 
 (defun display/init-pretty-fonts ()
   (use-package pretty-fonts
-    :init
-    (defconst pretty-fonts-hy-mode
-      '(("\\(self\\)"   ?⊙)))
+    :config (progn
+              (setq pretty-fonts-hy-font '(("\\(self\\)" ?⊙)))
 
-    :config
-    (progn
-      (pretty-fonts-set-kwds
-       '(;; Fira Code Ligatures
-         (pretty-fonts-fira-font prog-mode-hook org-mode-hook)
-         ;; Custom replacements not possible with `pretty-code' package
-         (pretty-fonts-hy-mode hy-mode-hook)))
+              (pretty-fonts-set-kwds
+               '(;; Fira Code Ligatures
+                 (pretty-fonts-fira-font prog-mode-hook org-mode-hook)
 
-      (pretty-fonts-set-fontsets
-       '(("fontawesome"
-          ;;                         
-          #xf07c #xf0c9 #xf0c4 #xf0cb #xf017 #xf101)
+                 ;; Other ligatures that are easier to define as simple
+                 ;; regexes so as to avoid `prettify-symbols-compose-predicate'
+                 (pretty-fonts-hy-font hy-mode-hook)))
 
-         ("all-the-icons"
-          ;;    
-          #xe907 #xe928)
+              (pretty-fonts-set-fontsets
+               '(
+                 ;; All-the-icons fontsets
+                 ("fontawesome"
+                  ;;                         
+                  #xf07c #xf0c9 #xf0c4 #xf0cb #xf017 #xf101)
 
-         ("github-octicons"
-          ;;                          
-          #xf091 #xf059 #xf076 #xf075 #xe192  #xf016)
+                 ("all-the-icons"
+                  ;;    
+                  #xe907 #xe928)
 
-         ("material icons"
-          ;;        
-          #xe871 #xe918 #xe3e7
-          ;;
-          #xe3d0 #xe3d1 #xe3d2 #xe3d4)
+                 ("github-octicons"
+                  ;;                          
+                  #xf091 #xf059 #xf076 #xf075 #xe192  #xf016)
 
-         ("Symbola"
-          ;; 𝕊    ⨂      ∅      ⟻    ⟼     ⊙      𝕋       𝔽
-          #x1d54a #x2a02 #x2205 #x27fb #x27fc #x2299 #x1d54b #x1d53d
-          ;; 𝔹    𝔇       𝔗
-          #x1d539 #x1d507 #x1d517)
-
-         ("Cantarell"
-          ;; ƒ
-          #x192))))))
+                 ("material icons"
+                  ;;        
+                  #xe871 #xe918 #xe3e7
+                  ;;              
+                  #xe3d0 #xe3d1 #xe3d2 #xe3d4))))))
 
 ;;;; Pretty-magit
 
 (defun display/init-pretty-magit ()
   (use-package pretty-magit
     :after ivy magit macros
-    :config
-    (progn
-      (pretty-magit-add-leader
-       "Feature"
-       ?
-       (:foreground "slate gray" :height 1.2))
 
-      (pretty-magit-add-leader
-       "Add"
-       ?
-       (:foreground "#375E97" :height 1.2))
+    :config (progn
+              (pretty-magit-add-leader
+               "Feature"
+               ?
+               (:foreground "slate gray" :height 1.2))
 
-      (pretty-magit-add-leader
-       "Fix"
-       ?
-       (:foreground "#FB6542" :height 1.2))
+              (pretty-magit-add-leader
+               "Add"
+               ?
+               (:foreground "#375E97" :height 1.2))
 
-      (pretty-magit-add-leader
-       "Clean"
-       ?
-       (:foreground "#FFBB00" :height 1.2))
+              (pretty-magit-add-leader
+               "Fix"
+               ?
+               (:foreground "#FB6542" :height 1.2))
 
-      (pretty-magit-add-leader
-       "Docs"
-       ?
-       (:foreground "#3F681C" :height 1.2))
+              (pretty-magit-add-leader
+               "Clean"
+               ?
+               (:foreground "#FFBB00" :height 1.2))
 
-      (pretty-magit-add-leader
-       "master"
-       ?
-       (:box t :height 1.2)
-       'no-prompt)
-
-      (pretty-magit-add-leader
-       "origin"
-       ?
-       (:box t :height 1.2)
-       'no-prompt))))
+              (pretty-magit-add-leader
+               "Docs"
+               ?
+               (:foreground "#3F681C" :height 1.2)))))
 
 ;;;; Pretty-outlines
 
 (defun display/init-pretty-outlines ()
   (use-package pretty-outlines
     :after outshine macros
-    :config
-    (progn
-      (setq pretty-outlines-bullets-bullet-list
-            '("" "" "" ""))
-      (setq pretty-outlines-ellipsis
-            "")
 
-      (spacemacs/add-to-hooks 'pretty-outlines-set-display-table
-                              '(outline-mode-hook
-                                outline-minor-mode-hook))
+    :config (progn
+              (setq pretty-outlines-ellipsis            "")
+              (setq pretty-outlines-bullets-bullet-list '("" "" "" ""))
 
-      (spacemacs/add-to-hooks 'pretty-outlines-add-bullets
-                              '(emacs-lisp-mode-hook
-                                hy-mode-hook
-                                python-mode-hook
-                                web-mode-hook)))))
+              (spacemacs/add-to-hooks 'pretty-outlines-set-display-table
+                                      '(outline-mode-hook
+                                        outline-minor-mode-hook))
 
-;;;; Windows-frame-size-fix
-
-;; (defun display/init-windows-frame-size-fix ()
-;;   (use-package windows-frame-size-fix
-;;     :if (not linux?)))
+              (spacemacs/add-to-hooks 'pretty-outlines-add-bullets
+                                      '(emacs-lisp-mode-hook
+                                        hy-mode-hook
+                                        python-mode-hook)))))
 
 ;;; Core Packages
 ;;;; All-the-icons
 
 (defun display/init-all-the-icons ()
   (use-package all-the-icons
-    :config
-    (progn
-      (defconst all-the-icons-icon-hy
-        '("\\.hy$" all-the-icons-fileicon "lisp" :face all-the-icons-orange))
-      (defconst all-the-icons-mode-icon-hy
-        '(hy-mode all-the-icons-fileicon "lisp" :face all-the-icons-orange))
-
-      (defconst all-the-icons-icon-graphviz
-        '("\\.dot$" all-the-icons-fileicon "graphviz" :face all-the-icons-pink))
-      (defconst all-the-icons-mode-icon-graphviz
-        '(graphviz-dot-mode all-the-icons-fileicon "graphviz" :face all-the-icons-pink))
-
-      (add-to-list 'all-the-icons-icon-alist all-the-icons-icon-hy)
-      (add-to-list 'all-the-icons-icon-alist all-the-icons-icon-graphviz)
-      (add-to-list 'all-the-icons-mode-icon-alist all-the-icons-mode-icon-hy)
-      (add-to-list 'all-the-icons-mode-icon-alist all-the-icons-mode-icon-graphviz))))
+    :config (progn
+              (add-to-list 'all-the-icons-icon-alist
+                           '("\\.hy$"
+                             all-the-icons-fileicon "lisp"
+                             :face all-the-icons-orange))
+              (add-to-list 'all-the-icons-icon-alist
+                           '("\\.dot$"
+                             all-the-icons-fileicon "graphviz"
+                             :face all-the-icons-pink))
+              (add-to-list 'all-the-icons-mode-icon-alist
+                           '(hy-mode
+                             all-the-icons-fileicon "lisp"
+                             :face all-the-icons-orange))
+              (add-to-list 'all-the-icons-mode-icon-alist
+                           '(graphviz-dot-mode
+                             all-the-icons-fileicon "graphviz"
+                             :face all-the-icons-pink)))))
 
 ;;;; All-the-icons-ivy
 
@@ -280,10 +211,10 @@
               (advice-add 'all-the-icons-ivy-file-transformer :override
                           'all-the-icons-ivy-file-transformer-stdized)
 
-              ;; Counsel defines a partiluar file transformer for just
-              ;; projectile. Replacing it with the all-the-icons-ivy one
-              ;; enables icons on projectile-find once-again. Yay.
-              (advice-add 'counsel-projectile-find-file-transformer :override
+              ;; Counsel defines a particular file transformer for just
+              ;; projectile (works on virtual files). Lets tack on the
+              ;; all-the-icons-ivy transformer for projectile icons once-again.
+              (advice-add 'counsel-projectile-find-file-transformer :filter-return
                           'all-the-icons-ivy-file-transformer-stdized)
 
               (all-the-icons-ivy-setup))))
@@ -298,16 +229,15 @@
 
 (defun display/init-pretty-mode ()
   (use-package pretty-mode
-    :config
-    (progn
-      (global-pretty-mode t)
+    :config (progn
+              (global-pretty-mode t)
 
-      (pretty-deactivate-groups
-       '(:equality :ordering :ordering-double :ordering-triple
-                   :arrows :arrows-twoheaded :punctuation
-                   :logic :sets))
-      (pretty-activate-groups
-       '(:greek :arithmetic-nary)))))
+              (pretty-deactivate-groups
+               '(:equality :ordering :ordering-double :ordering-triple
+                           :arrows :arrows-twoheaded :punctuation
+                           :logic :sets))
+              (pretty-activate-groups
+               '(:greek :arithmetic-nary)))))
 
 ;;;; Prettify-utils
 
@@ -320,10 +250,11 @@
   (spaceline-all-the-icons-theme)
 
   (setq spaceline-highlight-face-func 'spaceline-highlight-face-default)
-  (setq spaceline-all-the-icons-icon-set-modified 'chain)
+
+  (setq spaceline-all-the-icons-icon-set-modified         'chain)
   (setq spaceline-all-the-icons-icon-set-window-numbering 'square)
-  (setq spaceline-all-the-icons-separator-type 'none)
-  (setq spaceline-all-the-icons-primary-separator "")
+  (setq spaceline-all-the-icons-separator-type            'none)
+  (setq spaceline-all-the-icons-primary-separator         "")
 
   ;; Buffer Segments
   (spaceline-toggle-all-the-icons-buffer-size-off)
