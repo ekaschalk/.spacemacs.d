acb866c29315f8efe4814f65dcf161b2f32541ff
Eric Kaschalk <ekas@Erics-MacBook-Pro.local>
2018-10-11 11:25:15 -0700

    Docs: Update config layer readme, sync the keybindings table

--- layers/config/keybindings.el
+++ layers/config/keybindings.el
@@ -1,12 +1,14 @@
 ;; Package agnostic keybindings
 
-;; Windows commands
+;;; Windows commands
+
 (global-set-key
  (kbd "M-d") 'spacemacs/delete-window)
 (evil-define-key 'normal outline-minor-mode-map
   (kbd "C-M-i") 'spacemacs/alternate-buffer)  ; M-tab
 
-;; Outline navigation
+;;; Outline navigation
+
 (evil-define-key '(normal visual motion) outline-minor-mode-map
   "gh" 'outline-up-heading
   "gj" 'outline-forward-same-level
