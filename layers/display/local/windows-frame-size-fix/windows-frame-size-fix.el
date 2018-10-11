5bb32ac0e2887761e8d698887b407d2165d6eaf1
Eric Kaschalk <ekas@Erics-MacBook-Pro.local>
2018-10-11 14:07:08 -0700

    Clean: Remove windows-frame-size-fix display pkg

--- layers/display/local/windows-frame-size-fix/windows-frame-size-fix.el
+++
@@ -1,8 +0,0 @@
-;; Surface has 200% scaling, doesn't apply to emacs, f2 to fix init zooming
-
-(provide 'windows-frame-size-fix)
-
-;;; Windows-frame-size-fix
-
-(add-to-list 'default-frame-alist '(font . "operator mono medium"))
-(set-face-attribute 'default t :font "operator mono medium")
